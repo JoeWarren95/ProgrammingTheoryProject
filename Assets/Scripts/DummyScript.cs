b103a9ffeb1a5a5a9372e13fc5e38979625f2bb4
JoeWarren95 <jwarren1495@gmail.com>
2022-09-16 21:42:11 -0400

    Brain storm commit
    Created a couple folders, and brain stormed ideas for my "game"

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+   //THIS IS A DUMMY SCRIPT THAT IS SIMPLY FOR BRAINSTORMING
+   //IT WILL CONTAIN NO ACTUAL CODE
+   //I KNOW I PROBABLY SHOULDN'T BE USING A SCRIPT FOR THIS, 
+   //BUT IT HELPS ME THINK
+
+    /*
+     * ideas: vehicles, animals, buildings, shapes, dnd player types, types of computers, dungeon enemies
+     * 
+     *      - animal types: dog, horse, chicken
+     *              - can all Move(), Speak(), Sleep()
+     *      - dnd player types: warrior, wizard, bard
+     *              - can all Move(), Attack(), Rest()
+     *              - warrior: has a sword/shield, high health, mid attack, low magic
+     *              - wizard: has a magic staff, low health, low attack, high magic
+     *              - bard: has a lute for musci, has med health, mid attack, mid magic
+     *      - computer types: boomer, millenial, zoomer
+     *              - can all TurnOn(), RunProgram(), TurnOff()
+     *              - boomer: old HP computer with Windows 95, and a dino monitor (block)
+     *              - millenial: Dell computer with not such a block monitor, and respectable tower, not the latest n greatest, but affordable
+     *              - zoomer: new age computer "AlienWare", with a flat screen monitor and RGB lights
+     *      - dungeon enemy types: Zombie, Skeleton, Ogre
+     *              - Dungeon Enemy Log: will display stats (maybe bars?) once clicked on, and play little sound
+     *              - can all Move(), Attack(), Die()
+     *              - Zombie med speed, med attack, med health
+     *              - Skeleton high speed, med attack, low health
+     *              - Ogre slow speed, high attack, high health
+     * Top Down, 3D, Orthographic
+     */ 
+}
