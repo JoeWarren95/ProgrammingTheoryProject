d4d7fbad1a92acd340869c6b336f5e953fcfff15
JoeWarren95 <jwarren1495@gmail.com>
2022-09-17 22:40:21 -0400

    Created UI Script and Main Menu
    Main menu buttons work, transition between the game and exiting the app

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    //this script is responsible for handling all the UI in this game
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
